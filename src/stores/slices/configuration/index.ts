4e7f252297e727f863378071c0d896c0839472a0
Matheus Azambuja <matheushenrique.ads@gmail.com>
2023-06-28 08:38:43 -0300

    fix(configuration): default template to title

--- src/stores/slices/configuration/index.ts
+++ src/stores/slices/configuration/index.ts
@@ -37,7 +37,7 @@ const DEFAULT_STATE: IConfiguration = {
   obs_browser_cam_input_name: "",
   obs_browser_game_input_name: "",
   seo_title_template:
-    "NOME DO EVENTO - {game} [{category}] por <loop property='runners' separator=','>@{runners[primaryTwitch]}</loop>",
+    "NOME DO EVENTO - {game} [{category}] por <loop property='runners' separator=', '>@{runners[primaryTwitch]}</loop>",
   twitch_client_id: "",
   twitch_redirect_uri: "https://usina.spidium.live",
   twitch_token: "",
