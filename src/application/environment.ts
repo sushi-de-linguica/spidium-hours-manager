a08c88dc8a3c4bb6c56d28eb67148c3cc766d26e
Matheus Azambuja <matheushenrique.ads@gmail.com>
2023-08-05 19:11:11 -0300

    fix: environment to download correct database

--- src/application/environment.ts
+++ src/application/environment.ts
@@ -9,7 +9,7 @@ const environment: IEnvironment = {
     import.meta.env.MODE === "test" && import.meta.env.VITE_TEST == "true",
   testSufix: import.meta.env.VITE_TEST_SUFIX,
   SHM_DATABASE_URL:
-    "https://usina.spidium.live/shm-database/shm-database-v0_4_0.json",
+    "https://usina.spidium.live/shm-database/shm-database-v0_4_1.json",
 };
 
 export { environment };
